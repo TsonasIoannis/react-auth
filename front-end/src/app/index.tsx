21f4845b0e33b7c1e770e29ba0dcf694a2e6fd6e
TsonasIoannis <johntsonas@hotmail.com>
2021-09-26 00:51:09 +0100

    change workflow to yarn

--- front-end/src/app/index.tsx
+++ front-end/src/app/index.tsx
@@ -8,7 +8,7 @@
 
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Switch, Route, BrowserRouter, HashRouter } from 'react-router-dom';
 
 import { GlobalStyle } from 'styles/global-styles';
 
@@ -19,7 +19,7 @@ import { useTranslation } from 'react-i18next';
 export function App() {
   const { i18n } = useTranslation();
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Helmet
         titleTemplate="%s - React Boilerplate"
         defaultTitle="React Boilerplate"
@@ -33,6 +33,6 @@ export function App() {
         <Route component={NotFoundPage} />
       </Switch>
       <GlobalStyle />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
