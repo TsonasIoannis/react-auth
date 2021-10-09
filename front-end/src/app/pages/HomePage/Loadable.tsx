803d66e6c358c33cab041da69dedbc6d6a47a8cf
TsonasIoannis <johntsonas@hotmail.com>
2021-10-09 23:37:34 +0100

    add loading indicator component

--- front-end/src/app/pages/HomePage/Loadable.tsx
+++ front-end/src/app/pages/HomePage/Loadable.tsx
@@ -3,8 +3,25 @@
  */
 
 import { lazyLoad } from 'utils/loadable';
+import styled from 'styled-components/macro';
+import { LoadingIndicator } from 'app/components/LoadingIndicator';
+
+const LoadingWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 export const HomePage = lazyLoad(
   () => import('./index'),
   module => module.HomePage,
+  {
+    fallback: (
+      <LoadingWrapper>
+        <LoadingIndicator />
+      </LoadingWrapper>
+    ),
+  },
 );
